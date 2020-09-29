0ef8e26218b5f812544b69e0caac08b5ccb9a1c9
Nathan Sanford <nwsanfor24@gmail.com>
2020-09-29 18:13:21 -0400

    imported router and store into main.js in src folder

--- photobook-yt/src/main.js
+++ photobook-yt/src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 import './assets/tailwind.css'
 import Amplify from 'aws-amplify';
 import '@aws-amplify/ui-vue';
@@ -10,5 +12,7 @@ Amplify.configure(aws_exports);
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
